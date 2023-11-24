fbb9ca2321c52b15ca06c4461b4ede7f0a81834f
FALEJO94 <alejandro@MacBook-Pro-de-Alejandro.local>
2023-11-23 21:10:10 -0500

    primera version

--- main.py
+++ main.py
@@ -2,9 +2,10 @@ import streamlit as st
  
 st.title("Hola")
 
-st.header("Holaaaa")
+st.header("N.N")
 st.header("Este es una app Streamlist")
 st.subheader("Subheader")
 st.text("Texto")
 
 st.markdown("Markdown")
+
