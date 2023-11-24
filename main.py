f66503a0a77c9be88cc7520c224ef14b10f2c2af
FALEJO94 <alejandro@MacBook-Pro-de-Alejandro.local>
2023-11-23 21:11:13 -0500

    primera version

--- main.py
+++ main.py
@@ -9,3 +9,5 @@ st.text("Texto")
 
 st.markdown("Markdown")
 
+st.markdown("<h1 style='text-align: center;'>Métodos Numéricos y Optimización 🤘💥</h1>", unsafe_allow_html=True)
+st.markdown("<h3>Calculo de raices</h3><hr style='margin: 5px 0px;'>", unsafe_allow_html=True)
