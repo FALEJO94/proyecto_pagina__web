1519ad835442d2803a4b751185321d8f43e8649f
FALEJO94 <alejandro@MacBook-Pro-de-Alejandro.local>
2023-12-06 22:55:53 -0500

    v2

--- main.py
+++ main.py
@@ -1,14 +1,4 @@
 import streamlit as st
 
- 
-st.title("Hola")
-
-st.header("N.N.n.n")
-st.header("Este es una app Streamlist")
-st.subheader("Subheader")
-st.text("Texto")
-
-st.markdown("Markdown")
-
 st.markdown("<h1 style='text-align: center;'>Métodos Numéricos y Optimización 🤘💥</h1>", unsafe_allow_html=True)
 st.markdown("<h3>Calculo de raices</h3><hr style='margin: 5px 0px;'>", unsafe_allow_html=True)
